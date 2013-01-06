b9d701d15cf7c8cf5fb6f0c76befb080609347a9
Andy Stewart <lazycat.manatee@gmail.com>
2013-01-06 10:55:52 +0700

    Change with APIs of deepin_utils.

--- pkg_manager/apt/main.py
+++ pkg_manager/apt/main.py
@@ -21,7 +21,7 @@
 
 import sys
 import os
-from deepin_utils.path import get_parent_dir
+from deepin_utils.file import get_parent_dir
 sys.path.append(os.path.join(get_parent_dir(__file__, 3), "download_manager", "deepin_storm"))
 
 import tarfile
