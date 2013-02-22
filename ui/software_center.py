796564c5eb41da48b02d067e9374e204c5944229
Andy Stewart <lazycat.manatee@gmail.com>
2013-02-23 01:10:13 +0800

    Speed up ui start process.

--- ui/software_center.py
+++ ui/software_center.py
@@ -37,6 +37,7 @@ from dtk.ui.timeline import Timeline, CURVE_SINE
 from deepin_utils.process import run_command
 from deepin_utils.math_lib import solve_parabola
 from deepin_utils.file import read_file, write_file, touch_file
+from deepin_utils.multithread import create_thread
 from dtk.ui.utils import container_remove_all, set_cursor, get_widget_root_coordinate
 from dtk.ui.application import Application
 from dtk.ui.statusbar import Statusbar
@@ -445,27 +446,27 @@ class DeepinSoftwareCenter(dbus.service.Object):
         
         global_event.emit("show-message", "可以直接拖拽Deb文件到软件中心窗口进行安装哟. :)")
         
-    def run(self):    
-        log("Software center start")
+    def switch_page(self, page):
+        switch_page(self.page_switcher, self.page_box, page, self.detail_page)
         
-        # Init DBus.
-        system_bus = dbus.SystemBus()
-        bus_object = system_bus.get_object(DSC_SERVICE_NAME, DSC_SERVICE_PATH)
-        self.bus_interface = dbus.Interface(bus_object, DSC_SERVICE_NAME)
+    def show_home_page(self):
+        if self.detail_page and self.home_page:
+            self.switch_page(self.home_page)
+    
+    def show_upgrade_page(self):
+        if self.detail_page and self.upgrade_page:
+            self.switch_page(self.upgrade_page)
+    
+    def show_uninstall_page(self):
+        if self.detail_page and self.uninstall_page:
+            self.switch_page(self.uninstall_page)
+    
+    def show_install_page(self):
+        if self.detail_page and self.install_page:
+            self.switch_page(self.install_page)
         
-        log("Say hello to backend")
-        
-        # Say hello to backend. 
-        self.bus_interface.say_hello(self.simulate)
-        
-        log("Test deb files arguments")
-        
-        # Install deb file.
-        if len(self.deb_files) > 0:
-            self.bus_interface.install_deb_files(self.deb_files)
-        
-        log("Init application")
-        
+    def init_ui(self):
+        print "init ui"
         # Init application.
         self.application = Application(resizable=False)
         self.application.set_default_size(888, 634)
@@ -477,87 +478,50 @@ class DeepinSoftwareCenter(dbus.service.Object):
                 )
         self.application.window.set_title(_("Deepin Software Center"))
         
-        log("Init DND event for deb files")
-        
-        # Make window can received drop data.
-        targets = [("text/uri-list", 0, 1)]        
-        self.application.window.drag_dest_set(gtk.DEST_DEFAULT_MOTION | gtk.DEST_DEFAULT_DROP, targets, gtk.gdk.ACTION_COPY)
-        self.application.window.connect_after("drag-data-received", self.on_drag_data_received)        
-        
-        log("Init data manager")
-        
-        # Init data manager.
-        data_manager = DataManager(self.bus_interface)
-        
-        log("Init page box")
-        
         # Init page box.
-        page_box = gtk.VBox()
-        
-        log("Init detail view")
-        
-        # Init detail view.
-        detail_page = DetailPage(data_manager)
-        
-        log("Init page switcher")
+        self.page_box = gtk.VBox()
         
         # Init page switcher.
-        page_switcher = HSlider(200)
-        page_switcher.append_page(page_box)
-        page_switcher.append_page(detail_page)
-        page_switcher.set_to_page(page_box)
-        
-        log("Init page align")
+        self.page_switcher = HSlider(200)
+        self.page_switcher.append_page(self.page_box)
+        self.page_switcher.set_to_page(self.page_box)
         
         # Init page align.
         page_align = gtk.Alignment()
         page_align.set(0.5, 0.5, 1, 1)
         page_align.set_padding(0, 0, 2, 2)
         
-        log("Append page to switcher.")
-        
         # Append page to switcher.
-        page_align.add(page_switcher)
+        page_align.add(self.page_switcher)
         self.application.main_box.pack_start(page_align, True, True)
         
-        log("Init status bar.")
-        
         # Init status bar.
-        statusbar = Statusbar(24)
+        self.statusbar = Statusbar(24)
         status_box = gtk.HBox()
-        message_box = gtk.HBox()
+        self.message_box = gtk.HBox()
         join_us_button = LinkButton("加入我们", "http://www.linuxdeepin.com/joinus/job")
         join_us_button_align = gtk.Alignment()
         join_us_button_align.set(0.5, 0.5, 0, 0)
         join_us_button_align.set_padding(0, 3, 0, 10)
         join_us_button_align.add(join_us_button)
-        status_box.pack_start(message_box, True, True)
+        status_box.pack_start(self.message_box, True, True)
         status_box.pack_start(join_us_button_align, False, False)
-        statusbar.status_box.pack_start(status_box, True, True)
-        self.application.main_box.pack_start(statusbar, False, False)
-        
-        log("Init pages.")
-        
-        # Init pages.
-        log("Init home page.")
-        home_page = HomePage(data_manager)
-        log("Init upgrade page.")
-        upgrade_page = UpgradePage(self.bus_interface, data_manager)
-        log("Init uninstall page.")
-        uninstall_page = UninstallPage(self.bus_interface, data_manager)
-        log("Init install page.")
-        install_page = InstallPage(self.bus_interface, data_manager)
-        log("Init switch page.")
-        switch_page(page_switcher, page_box, home_page, detail_page)
-        
-        log("Init navigatebar.")
+        self.statusbar.status_box.pack_start(status_box, True, True)
+        self.application.main_box.pack_start(self.statusbar, False, False)
         
         # Init navigatebar.
-        navigatebar = Navigatebar(
-                [(app_theme.get_pixbuf("navigatebar/nav_recommend.png"), " 软件中心", lambda : switch_page(page_switcher, page_box, home_page, detail_page)),
-                (app_theme.get_pixbuf("navigatebar/nav_update.png"), " 系统升级", lambda : switch_page(page_switcher, page_box, upgrade_page, detail_page)),
-                (app_theme.get_pixbuf("navigatebar/nav_uninstall.png"), " 卸载软件", lambda : switch_page(page_switcher, page_box, uninstall_page, detail_page)),
-                (app_theme.get_pixbuf("navigatebar/nav_download.png"), " 安装管理", lambda : switch_page(page_switcher, page_box, install_page, detail_page)),
+        self.detail_page = None
+        self.home_page = None
+        self.upgrade_page = None
+        self.uninstall_page = None
+        self.install_page = None
+        
+        self.navigatebar = Navigatebar(
+                [
+                (app_theme.get_pixbuf("navigatebar/nav_recommend.png"), " 软件中心", self.show_home_page),
+                (app_theme.get_pixbuf("navigatebar/nav_update.png"), " 系统升级", self.show_upgrade_page),
+                (app_theme.get_pixbuf("navigatebar/nav_uninstall.png"), " 卸载软件", self.show_uninstall_page),
+                (app_theme.get_pixbuf("navigatebar/nav_download.png"), " 安装管理", self.show_install_page),
                 ],
                 font_size = 11,
                 padding_x = 2,
@@ -566,16 +530,13 @@ class DeepinSoftwareCenter(dbus.service.Object):
                 item_hover_pixbuf=app_theme.get_pixbuf("navigatebar/nav_hover.png"),
                 item_press_pixbuf=app_theme.get_pixbuf("navigatebar/nav_press.png"),
                 )
-        navigatebar.set_size_request(-1, 56)
-        navigatebar_align = gtk.Alignment(0, 0, 1, 1)
-        navigatebar_align.set_padding(0, 0, 4, 0)
-        navigatebar_align.add(navigatebar)
+        self.navigatebar.set_size_request(-1, 56)
+        self.navigatebar_align = gtk.Alignment(0, 0, 1, 1)
+        self.navigatebar_align.set_padding(0, 0, 4, 0)
+        self.navigatebar_align.add(self.navigatebar)
         self.application.titlebar.set_size_request(-1, 56)
-        self.application.titlebar.left_box.pack_start(navigatebar_align, True, True)
-        self.application.window.add_move_event(navigatebar)
-        self.application.window.connect("show", lambda w: request_status(self.bus_interface, install_page, upgrade_page, uninstall_page))
-        
-        log("Init menu.")
+        self.application.titlebar.left_box.pack_start(self.navigatebar_align, True, True)
+        self.application.window.add_move_event(self.navigatebar)
         
         # Init menu.
         menu = Menu(
@@ -595,40 +556,97 @@ class DeepinSoftwareCenter(dbus.service.Object):
                 get_widget_root_coordinate(button, WIDGET_POS_BOTTOM_LEFT),
                 (button.get_allocation().width, 0)))
         
+        # Make window can received drop data.
+        targets = [("text/uri-list", 0, 1)]        
+        self.application.window.drag_dest_set(gtk.DEST_DEFAULT_MOTION | gtk.DEST_DEFAULT_DROP, targets, gtk.gdk.ACTION_COPY)
+        self.application.window.connect_after("drag-data-received", self.on_drag_data_received)        
+        
+        self.application.window.connect_after("show", lambda w: gtk.timeout_add(500, lambda : create_thread(self.init_backend).start()))
+        
+        self.application.run()
+        
+    def init_backend(self):
+        log("Say hello to backend")
+        
+        # Init DBus.
+        system_bus = dbus.SystemBus()
+        bus_object = system_bus.get_object(DSC_SERVICE_NAME, DSC_SERVICE_PATH)
+        self.bus_interface = dbus.Interface(bus_object, DSC_SERVICE_NAME)
+        
+        # Say hello to backend. 
+        self.bus_interface.say_hello(self.simulate)
+        
+        log("Test deb files arguments")
+        
+        # Install deb file.
+        if len(self.deb_files) > 0:
+            self.bus_interface.install_deb_files(self.deb_files)
+        
+        log("Init data manager")
+        
+        # Init data manager.
+        data_manager = DataManager(self.bus_interface)
+        
+        log("Init detail view")
+        
+        # Init detail view.
+        self.detail_page = DetailPage(data_manager)
+        
+        self.page_switcher.append_page(self.detail_page)
+        
+        log("Init pages.")
+        
+        # Init pages.
+        log("Init home page.")
+        self.home_page = HomePage(data_manager)
+        log("Init upgrade page.")
+        self.upgrade_page = UpgradePage(self.bus_interface, data_manager)
+        log("Init uninstall page.")
+        self.uninstall_page = UninstallPage(self.bus_interface, data_manager)
+        log("Init install page.")
+        self.install_page = InstallPage(self.bus_interface, data_manager)
+        log("Init switch page.")
+        self.switch_page(self.home_page)
+        
+        self.application.window.connect("show", lambda w: request_status(self.bus_interface, self.install_page, self.upgrade_page, self.uninstall_page))
+        
         log("Handle global event.")
         
         # Handle global event.
-        global_event.register_event("install-pkg", lambda pkg_names: install_pkg(self.bus_interface, install_page, pkg_names, self.application.window))
+        global_event.register_event("install-pkg", lambda pkg_names: install_pkg(self.bus_interface, self.install_page, pkg_names, self.application.window))
         global_event.register_event("upgrade-pkg", self.bus_interface.upgrade_pkg)
         global_event.register_event("uninstall-pkg", self.bus_interface.uninstall_pkg)
         global_event.register_event("stop-download-pkg", self.bus_interface.stop_download_pkg)
-        global_event.register_event("switch-to-detail-page", lambda pkg_name : switch_to_detail_page(page_switcher, detail_page, pkg_name))
-        global_event.register_event("switch-from-detail-page", lambda : switch_from_detail_page(page_switcher, detail_page, page_box))
+        global_event.register_event("switch-to-detail-page", lambda pkg_name : switch_to_detail_page(self.page_switcher, self.detail_page, pkg_name))
+        global_event.register_event("switch-from-detail-page", lambda : switch_from_detail_page(self.page_switcher, self.detail_page, self.page_box))
         global_event.register_event("remove-wait-action", self.bus_interface.remove_wait_missions)
         global_event.register_event("remove-wait-download", self.bus_interface.remove_wait_downloads)
         global_event.register_event("request-clear-action-pages", request_clear_action_pages)
         global_event.register_event("request-stop-install-actions", request_stop_install_actions)
         global_event.register_event("request-clear-failed-action", request_clear_failed_action)
-        global_event.register_event("update-upgrade-notify-number", lambda number: update_navigatebar_number(navigatebar, 1, number))        
-        global_event.register_event("update-install-notify-number", lambda number: update_navigatebar_number(navigatebar, 3, number))        
+        global_event.register_event("update-upgrade-notify-number", lambda number: update_navigatebar_number(self.navigatebar, 1, number))        
+        global_event.register_event("update-install-notify-number", lambda number: update_navigatebar_number(self.navigatebar, 3, number))        
         global_event.register_event("jump-to-category", 
                                     lambda first_category_name, second_category_name: 
-                                    jump_to_category(page_switcher, page_box, home_page, detail_page, first_category_name, second_category_name))
+                                    jump_to_category(self.page_switcher, 
+                                                     self.page_box, 
+                                                     self.home_page, 
+                                                     self.detail_page, 
+                                                     first_category_name, 
+                                                     second_category_name))
         global_event.register_event("grade-pkg", lambda pkg_name, star: grade_pkg(self.application.window, pkg_name, star))
         global_event.register_event("set-cursor", lambda cursor: set_cursor(self.application.window, cursor))
-        global_event.register_event("show-message", lambda message: show_message(statusbar, message_box, message))
+        global_event.register_event("show-message", lambda message: show_message(self.statusbar, self.message_box, message))
         global_event.register_event("start-pkg", lambda pkg_name, desktop_infos, offset: start_pkg(pkg_name, desktop_infos, offset, self.application.window))
         global_event.register_event("start-desktop", start_desktop)
-        system_bus.add_signal_receiver(lambda messages: message_handler(messages, self.bus_interface, upgrade_page, uninstall_page, install_page),
+        system_bus.add_signal_receiver(lambda messages: message_handler(messages, self.bus_interface, self.upgrade_page, self.uninstall_page, self.install_page),
                                        dbus_interface=DSC_SERVICE_NAME, path=DSC_SERVICE_PATH, signal_name="update_signal")
-        glib.timeout_add(1000, lambda : clear_action_pages(self.bus_interface, upgrade_page, uninstall_page, install_page))
-        glib.timeout_add(1000, lambda : clear_install_stop_list(install_page))
-        glib.timeout_add(1000, lambda : clear_failed_action(install_page, upgrade_page))
+        glib.timeout_add(1000, lambda : clear_action_pages(self.bus_interface, self.upgrade_page, self.uninstall_page, self.install_page))
+        glib.timeout_add(1000, lambda : clear_install_stop_list(self.install_page))
+        glib.timeout_add(1000, lambda : clear_failed_action(self.install_page, self.upgrade_page))
         
-        log("Run")
-        
-        # Run.
-        self.application.run()
+    def run(self):    
+        self.init_ui()
         
         log("Send exit request to backend when frontend exit.")
         
