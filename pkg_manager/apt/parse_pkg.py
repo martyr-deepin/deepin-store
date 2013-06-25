0cda5875068a5119c6f40c0021550226016311da
iceleaf916 <kaisheng.ye@gmail.com>
2013-06-25 16:33:59 +0800

    fix bug in parse_pkg for 64bit system

--- pkg_manager/apt/parse_pkg.py
+++ pkg_manager/apt/parse_pkg.py
@@ -202,31 +202,30 @@ def check_hash(path, hash_type, hash_value):
     return hash_fun.hexdigest() == hash_value
 
 def get_pkg_dependence_file_path(cache, pkg_name):
+
     cache_archive_dir = get_cache_archive_dir()
     file_paths = []
-    if pkg_name in cache:
+    try:
+        pkg = cache[pkg_name]
+    except:
         try:
+            pkg_name = pkg_name + ":i386"
             pkg = cache[pkg_name]
-            if cache.is_pkg_upgradable(pkg_name):
-                pkg.mark_upgrade()
-            elif not cache.is_pkg_installed(pkg_name):
-                pkg.mark_install()
-                
-            # Get package information.
-            pkgs = sorted(cache.get_changes(), key=lambda pkg: pkg.name)
-            cache._depcache.init()
-            file_paths.append(os.path.join(cache_archive_dir, get_filename(pkg.candidate)))
-            for pkg in pkgs:
-                file_paths.append(os.path.join(cache_archive_dir, get_filename(pkg.candidate)))
+        except:
             return file_paths
-        
-        except Exception, e:
-            print "get_pkg_download_info error: %s" % (e)
-            log(str(traceback.format_exc()))
 
-            return []
-    else:
-        raise Exception("%s is not found" % pkg_name)
+    if cache.is_pkg_upgradable(pkg_name):
+        pkg.mark_upgrade()
+    elif not cache.is_pkg_installed(pkg_name):
+        pkg.mark_install()
+        
+    # Get package information.
+    pkgs = sorted(cache.get_changes(), key=lambda pkg: pkg.name)
+    cache._depcache.init()
+    file_paths.append(os.path.join(cache_archive_dir, get_filename(pkg.candidate)))
+    for pkg in pkgs:
+        file_paths.append(os.path.join(cache_archive_dir, get_filename(pkg.candidate)))
+    return file_paths
 
 if __name__ == "__main__":
     from apt_cache import AptCache
@@ -239,5 +238,5 @@ if __name__ == "__main__":
     # print deb_package.check_breaks_existing_packages()
     # print deb_package.check_conflicts()
 
-    for path in get_pkg_dependence_file_path(cache, "apache2"):
+    for path in get_pkg_dependence_file_path(cache, "kingsoft-office"):
         print path
