c06bebf5a1a4396aa239f1e70dce85dbb9db8e7a
yekaisheng <kaisheng.ye@gmail.com>
2015-01-09 11:19:06 +0800

    remove deepin-storm dependence
    Change-Id: I34fc2793f0f3358fcb4c40e814baa243fe3e2d1c

--- pkg_manager/apt/download_manager.py
+++ pkg_manager/apt/download_manager.py
@@ -22,7 +22,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from deepin_storm.logger import Logger
+from logger import Logger
 
 import sys
 import dbus
